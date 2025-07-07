3e45359b77105bc0ba979edd799b04f2072b5e74
Cagatay Gedik <cagatay.gedik@icloud.com>
2025-07-07 15:40:00 +0300

    Initial Commit

---
+++ MVVM-R/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-R
+//
+//  Created by Celil Çağatay Gedik on 7.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
