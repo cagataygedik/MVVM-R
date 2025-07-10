e022d88c456f859d3e058ff11d66e2fc410ccc05
Cagatay Gedik <cagatay.gedik@icloud.com>
2025-07-10 13:27:46 +0300

    created data models

---
+++ MVVM-R/Car.swift
@@ -0,0 +1,25 @@
+//
+//  Car.swift
+//  MVVM-R
+//
+//  Created by Celil Çağatay Gedik on 10.07.2025.
+//
+
+import Foundation
+
+struct Car: Identifiable, Codable {
+    var id = UUID()
+    let brand: String
+    let model: String
+    let year: Int
+    let price: Double
+    let imageURL: String
+    let description: String
+    let mileage: Int
+    let fuelType: String
+    let transmission: String
+    
+    var formattedPrice: String {
+        return String(format: "$%.0f", price)
+    }
+}
---
+++ MVVM-R/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  MVVM-R
+//
+//  Created by Celil Çağatay Gedik on 10.07.2025.
+//
+
+import Foundation
+
+struct User: Codable {
+    let username: String
+    let email: String
+}
