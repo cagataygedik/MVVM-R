896b121c92e2e82ae38f38d39dae999b99c489b4
Cagatay Gedik <cagatay.gedik@icloud.com>
2025-07-10 13:28:44 +0300

    created Route enum

---
+++ MVVM-R/Route.swift
@@ -0,0 +1,14 @@
+//
+//  Route.swift
+//  MVVM-R
+//
+//  Created by Celil Çağatay Gedik on 10.07.2025.
+//
+
+import Foundation
+
+enum Route {
+    case login
+    case main
+    case carDetail(Car)
+}
